1742360d183690fc81cb0acb374a1a3269646ba2
pashkovskyi_maksym <pashkovskymax.mp@gmail.com>
2021-03-19 19:18:18 +0200

    first commit

--- test/initial.js
+++ test/initial.js
@@ -36,71 +36,76 @@ describe('My Little Hero', function () { //define suite title by passing a strin
             expect(input).toEqual(true);
         });
 
+        it('TC-006 Gender radio buttons  label is present', function (){
+            let input= $$('.ant-form-item-required')[1].isDisplayed();
+            expect(input).toEqual(true);
+        });
+
         describe('Gender radio buttons', function (){    //define sub-suite title by passing a string
 
-            it('TC-006 Gender radio buttons label (he) is present', function (){
+            it('TC-007 Gender radio buttons label (he) is present', function (){
                 let label = $$('.ant-radio-wrapper')[0].isDisplayed();
                 expect(label).toEqual(true);
             });
 
-            it('TC-007 Gender radio buttons label (she) is present', function (){
+            it('TC-008 Gender radio buttons label (she) is present', function (){
                 let label = $$('.ant-radio-wrapper')[1].isDisplayed();
                 expect(label).toEqual(true);
             });
 
-            it('TC-008 Gender radio buttons label (it) is present', function (){
+            it('TC-009 Gender radio buttons label (it) is present', function (){
                 let label = $$('.ant-radio-wrapper')[2].isDisplayed();
                 expect(label).toEqual(true);
             });
 
-            it('TC-009 Gender radio buttons (he) is present', function (){
+            it('TC-010 Gender radio buttons (he) is present', function (){
                 let radioButtons = $$('.ant-radio')[0].isDisplayed();
                 expect(radioButtons).toEqual(true)
             });
 
-            it('TC-010 Gender radio buttons (she) is present', function (){
+            it('TC-011 Gender radio buttons (she) is present', function (){
                 let radioButtons = $$('.ant-radio')[1].isDisplayed();
                 expect(radioButtons).toEqual(true)
             });
 
-            it('TC-011 Gender radio buttons (it) is present', function (){
+            it('TC-012 Gender radio buttons (it) is present', function (){
                 let radioButtons = $$('.ant-radio')[2].isDisplayed();
                 expect(radioButtons).toEqual(true)
             });
 
         })
 
-        it('TC-012 Age field  label is present', function (){
+        it('TC-013 Age field  label is present', function (){
             let label = $$('.ant-form-item-required')[2].isDisplayed();
             expect(label).toEqual(true)
         });
 
-        it('TC-013 Age field  is present', function (){
+        it('TC-014 Age field  is present', function (){
             let input= $$('.ant-input')[1].isDisplayed();
             expect(input).toEqual(true);
         });
 
-        it('TC-014 Story Type dropdown label is present', function (){
+        it('TC-015 Story Type dropdown label is present', function (){
             let label = $$('.ant-form-item-required')[3].isDisplayed();
             expect(label).toEqual(true)
         });
 
-        it('TC-015 Story Type dropdown is present', function (){
+        it('TC-016 Story Type dropdown is present', function (){
             let dropdown = $('.ant-select-selector').isDisplayed();
             expect(dropdown).toEqual(true)
         });
 
-        it('TC-016 Image field  label is present', function (){
+        it('TC-017 Image field  label is present', function (){
             let label = $('label[title="5. Upload an image (optional)"]').isDisplayed();
             expect(label).toEqual(true)
         });
 
-        it('TC-017 Image field  is present', function (){
+        it('TC-018 Image field  is present', function (){
             let label = $('.ant-upload').isDisplayed();
             expect(label).toEqual(true)
         });
 
-        it('TC-018 Submit button  is present', function (){
+        it('TC-019 Submit button  is present', function (){
             let button = $('.ant-btn').isDisplayed();
             expect(button).toEqual(true)
         });
