085d8c9351a83d08c2db0c9523ede518cfdecce3
MaksymPashkovskyi <pashkovskymax.mp@gmail.com>
2021-03-21 01:07:28 +0200

    smoke tests

--- data/selectors.js
+++ data/selectors.js
@@ -4,11 +4,14 @@ const elements = {
     "requiredLabel": ".ant-form-item-required", // "label[for=name]","label[for=age]","label[for=gender]"
     "name": "#name",
     "radioButtons": ".ant-radio",
+    "radioButtonsLabel": ".ant-radio-wrapper",
     "age": "#age",
     "storyType": ".ant-select-selector",
+    "storyList": ".ant-select-item",
     "imageLabel": ".ant-form-item-label",
     "image": "span.ant-upload",
-    "submitButton": ".ant-btn"
+    "submitButton": ".ant-btn",
+    "tryAgain": ".btn-primary"
 }
 
 module.exports = elements;
