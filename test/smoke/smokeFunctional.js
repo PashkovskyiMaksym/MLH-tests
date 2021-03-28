35d1ab1f7e0b2eacaf928f25df0c4ba20bc18f71
MaksymPashkovskyi <pashkovskymax.mp@gmail.com>
2021-03-28 12:59:24 +0300

    add function TC-027

--- test/smoke/smokeFunctional.js
+++ test/smoke/smokeFunctional.js
@@ -1,5 +1,6 @@
 import sel from '../../data/selectors'
 import {name, gender,  age, story} from '../../data/testdata'
+import inputValues4 from '../../helpers/methods'
 
 describe('Required fields and story created', function () {
 
@@ -13,21 +14,14 @@ describe('Required fields and story created', function () {
         $(sel.age).setValue(age.default);
         $(sel.storyType).click();
         $$(sel.storyList)[story.comedy].click();
-        browser.pause(2000);
         let submitBtn = $(sel.submitButton).isEnabled();
         expect(submitBtn).toEqual(true);
     });
 
     it('TC-027 Submit button is enabled after fields 1-4 are field in with valid values', function () {
         browser.refresh();
-
-        $(sel.name).setValue('LadyBug007');
-        $$(sel.radioButtons)[1].click();
-        $(sel.age).setValue('1234567890');
-        $(sel.storyType).click();
-        $$(sel.storyList)[6].click();
+        inputValues4(name.default, gender.she, age.default, story.comedy);
         $(sel.submitButton).click();
-        browser.pause(2000)
         let tryAgainButton = $(sel.tryAgain).isDisplayed();
         expect(tryAgainButton).toEqual(true);
     });
